17e898ac1e0cd8d1b8cad8b96403d2d36637d8ea
Jasper Haag <jasperhaag16@gmail.com>
2018-04-13 12:24:41 -0400

    fix to rust mode config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,7 +20,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/"))
+	     '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
--- .emacs.d/languages/j-rust.el
+++ .emacs.d/languages/j-rust.el
@@ -1,5 +1,7 @@
-(use-package rust
-  :ensure rust-mode
-  :defer t)
+(use-package rust-mode
+  :ensure t
+  :defer t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode)))
 
 (provide 'j-rust)
