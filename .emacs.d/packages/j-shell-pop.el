8e26dbf228da86674af71eeee96c8577247bce57
Jasper Haag <jasperhaag16@gmail.com>
2018-06-06 13:12:41 -0400

    Revamp shell-pop to work properly and support xterm 256 colors

--- .emacs.d/packages/j-shell-pop.el
+++ .emacs.d/packages/j-shell-pop.el
@@ -1,11 +1,26 @@
+; Add support for 256 colors to ansi-term
+(use-package eterm-256color
+  :ensure t
+  :config
+  (add-hook 'term-mode-hook #'eterm-256color-mode))
+
 (use-package shell-pop
   :ensure t
-  :defer t
-  :bind ("M-+" . shell-pop)
-  :config (progn
-            (custom-set-variables
-             '(shell-pop-shell-type "ansi-term"))
-            (add-hook 'shell-pop-out-hook (lambda ()
-                                            (delete-process "*shell-1*")))))
+  :after (xterm-color eterm-256color)
+  :bind ([f1] . shell-pop)
+  :custom
+  (shell-pop-window-size 40)
+  (shell-pop-full-span t)
+  (shell-pop-window-position "bottom")
+  (shell-pop-default-directory (getenv "HOME"))
+  (shell-pop-autocd-to-working-dir nil)
+  :config
+  (shell-pop--set-shell-type 'ansi-term '("ansi-term"
+                                          "*ansi-term*"
+                                          (lambda ()
+                                            (ansi-term shell-pop-term-shell))))
+  (add-hook 'shell-pop-in-after-hook
+            (lambda ()
+              (set-process-query-on-exit-flag (get-process "*ansi-term*") nil))))
 
 (provide 'j-shell-pop)
