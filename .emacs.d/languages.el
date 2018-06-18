347bca58bccfe13cb064b4ab00af05dc24051b9a
Jasper Haag <jasperhaag16@gmail.com>
2018-06-18 11:31:33 -0400

    Setup js2-mode for emacs

--- .emacs.d/languages.el
+++ .emacs.d/languages.el
@@ -1,6 +1,7 @@
 ;;=== Custom Major Mode Associations ===========================================
 (add-to-list 'auto-mode-alist '("^\\.\\w.*$" . prog-mode))
 
-(require 'j-haskell)
 (require 'j-asm)
+(require 'j-haskell)
+(require 'j-js)
 (require 'j-rust)
---
+++ .emacs.d/languages/j-js.el
@@ -0,0 +1,30 @@
+(use-package js2-mode
+  :defer t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+  :custom
+  (js2-basic-offset 2)
+  (js-indent-level 2))
+
+(use-package js2-refactor
+  :after js2-mode
+  :defer t
+  :init
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+  :config
+  (js2r-add-keybindings-with-prefix "C-c C-r"))
+
+(use-package xref-js2
+  :after js2-mode
+  :defer t
+  :init
+  (define-key js-mode-map (kbd "M-.") nil)
+  (add-hook 'js2-mode-hook (lambda ()
+                             (add-hook 'xref-backend-functions
+                                       #'xref-js2-xref-backend
+                                       nil
+                                       t))))
+
+(provide 'j-js)
