d3ea9267d30bf00dd7032275bcda30b9a4405de6
Jasper Haag <jasperhaag16@gmail.com>
2018-12-10 13:40:21 -0500

    added auto-format for rust

--- .emacs.d/languages/j-rust.el
+++ .emacs.d/languages/j-rust.el
@@ -2,6 +2,7 @@
   :ensure t
   :defer t
   :init
-  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode)))
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+  (setq rust-format-on-save t))
 
 (provide 'j-rust)
